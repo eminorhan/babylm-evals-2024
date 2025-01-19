453dbb01b1a348ff02a3ec96348e47ca8f164110
eminorhan <aeminorhan@gmail.com>
2025-01-18 23:52:26 -0500

    update eval_blimp.sh to include all models

--- eval_blimp.sh
+++ eval_blimp.sh
@@ -3,31 +3,44 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:a100:1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=240GB
-#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=62GB
+#SBATCH --time=08:00:00
 #SBATCH --job-name=eval_blimp
 #SBATCH --output=eval_blimp_%A_%a.out
-#SBATCH --array=1-12
+#SBATCH --array=0-25
 
-# Define the base path for models
-BASE_MODEL=babylm_10M_llama
-BASE_PATH=/vast/eo41/babylm/models/${BASE_MODEL}
+# define models
+MODEL_NAMES=(
+    "babylm_10M"
+    "wikipedia_10M_1" "wikipedia_10M_2" "wikipedia_10M_3"
+    "gutenberg_10M_1" "gutenberg_10M_2" "gutenberg_10M_3"
+    "tinystories_10M_1" "tinystories_10M_2" "tinystories_10M_3"
+    "pythonedu_10M_1" "pythonedu_10M_2" "pythonedu_10M_3"
+    "babylm_100M"
+    "wikipedia_100M_1" "wikipedia_100M_2" "wikipedia_100M_3"
+    "gutenberg_100M_1" "gutenberg_100M_2" "gutenberg_100M_3"
+    "tinystories_100M_1" "tinystories_100M_2" "tinystories_100M_3"
+    "pythonedu_100M_1" "pythonedu_100M_2" "pythonedu_100M_3"
+)
 
-# Compute the step number based on the array index
-STEP=$((100 * SLURM_ARRAY_TASK_ID))
+# get current model name based on array index
+MODEL_NAME=${MODEL_NAMES[$SLURM_ARRAY_TASK_ID]}
+BASE_MODEL_PATH=/scratch/eo41/babylm/models/${MODEL_NAME}
 
-# Construct the model path
-MODEL_PATH=${BASE_PATH}/step_${STEP}
-MODEL_BASENAME=$(basename $MODEL_PATH)
+# loop through step directories within current model
+for FULL_MODEL_PATH in ${BASE_MODEL_PATH}/step_*; do
 
-# Execute the evaluation script
-srun python -u lm_eval --model hf \
-    --model_args pretrained=$MODEL_PATH,dtype=bfloat16 \
-    --tasks blimp_filtered,blimp_supplement \
-    --device cuda:0 \
-    --batch_size 16 \
-    --output_path results/blimp/${BASE_MODEL}/${MODEL_BASENAME}/blimp_results.json
+    echo "Model: ${FULL_MODEL_PATH}"
 
-# use `--model_args pretrained=$MODEL_PATH,backend="mlm"` if you're using a custom masked LM
-# add --trust_remote_code if you need to load custom config/model files
+    # extract step name (e.g., step_500)
+    STEP_NAME=$(basename "$FULL_MODEL_PATH")
+
+    # execute eval
+    srun python -u lm_eval --model hf \
+        --model_args pretrained=$FULL_MODEL_PATH,dtype=bfloat16 \
+        --tasks blimp_filtered,blimp_supplement \
+        --device cuda:0 \
+        --batch_size 16 \
+        --output_path results/blimp/${MODEL_NAME}/${STEP_NAME}/blimp_results.json
+done
