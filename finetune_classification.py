ad182d72a36be4f3b1c2a07d1aeb6cfef306dd24
eminorhan <aeminorhan@gmail.com>
2025-01-25 23:23:47 -0500

    add glue eval

--- finetune_classification.py
+++ finetune_classification.py
@@ -80,87 +80,19 @@ class DataTrainingArguments:
     the command line.
     """
 
-    task_name: Optional[str] = field(
-        default=None,
-        metadata={"help": "The name of the task to train on: " + ", ".join(task_to_keys.keys())},
-    )
-    dataset_name: Optional[str] = field(
-        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
-    )
-    dataset_config_name: Optional[str] = field(
-        default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)."}
-    )
-    max_seq_length: int = field(
-        default=128,
-        metadata={
-            "help": (
-                "The maximum total input sequence length after tokenization. Sequences longer "
-                "than this will be truncated, sequences shorter will be padded."
-            )
-        },
-    )
-    overwrite_cache: bool = field(
-        default=False, metadata={"help": "Overwrite the cached preprocessed datasets or not."}
-    )
-    pad_to_max_length: bool = field(
-        default=True,
-        metadata={
-            "help": (
-                "Whether to pad all samples to `max_seq_length`. "
-                "If False, will pad the samples dynamically when batching to the maximum length in the batch."
-            )
-        },
-    )
-    max_train_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of training examples to this "
-                "value if set."
-            )
-        },
-    )
-    max_eval_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
-                "value if set."
-            )
-        },
-    )
-    max_predict_samples: Optional[int] = field(
-        default=None,
-        metadata={
-            "help": (
-                "For debugging purposes or quicker training, truncate the number of prediction examples to this "
-                "value if set."
-            )
-        },
-    )
-    patience: Optional[int] = field(
-       default=None,
-       metadata={
-            "help": (
-                "Number of evaluation steps without improvement > epsilon before stopping fine-tuning. "
-                "Requires the use of the --eval_every argument."
-            )
-       },
-    )
-    eval_every: Optional[int] = field(
-        default=None,
-        metadata = {
-            "help": (
-                "Number of steps between evaluations (MUST be set if patience is set)."
-            )
-        }
-    )
-    train_file: Optional[str] = field(
-        default=None, metadata={"help": "A csv or a json file containing the training data."}
-    )
-    validation_file: Optional[str] = field(
-        default=None, metadata={"help": "A csv or a json file containing the validation data."}
-    )
+    task_name: Optional[str] = field(default=None, metadata={"help": "The name of the task to train on: " + ", ".join(task_to_keys.keys())})
+    dataset_name: Optional[str] = field(default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."})
+    dataset_config_name: Optional[str] = field(default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)."})
+    max_seq_length: int = field(default=128, metadata={"help": "The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded."})
+    overwrite_cache: bool = field(default=False, metadata={"help": "Overwrite the cached preprocessed datasets or not."})
+    pad_to_max_length: bool = field(default=True, metadata={"help": "Whether to pad all samples to `max_seq_length`. If False, will pad the samples dynamically when batching to the maximum length in the batch."})
+    max_train_samples: Optional[int] = field(default=None, metadata={"help": "For debugging purposes or quicker training, truncate the number of training examples to this value if set."})
+    max_eval_samples: Optional[int] = field(default=None, metadata={"help": "For debugging purposes or quicker training, truncate the number of evaluation examples to this value if set."})
+    max_predict_samples: Optional[int] = field(default=None, metadata={"help": "For debugging purposes or quicker training, truncate the number of prediction examples to this value if set."})
+    patience: Optional[int] = field(default=None, metadata={"help": "Number of evaluation steps without improvement > epsilon before stopping fine-tuning. Requires the use of the --eval_every argument."})
+    eval_every: Optional[int] = field(default=None, metadata = {"help": "Number of steps between evaluations (MUST be set if patience is set)."})
+    train_file: Optional[str] = field(default=None, metadata={"help": "A csv or a json file containing the training data."})
+    validation_file: Optional[str] = field(default=None, metadata={"help": "A csv or a json file containing the validation data."})
     test_file: Optional[str] = field(default=None, metadata={"help": "A csv or a json file containing the test data."})
 
     def __post_init__(self):
@@ -176,9 +108,7 @@ class DataTrainingArguments:
             train_extension = self.train_file.split(".")[-1]
             assert train_extension in ["csv", "json", "jsonl"], "`train_file` should be a csv or a json file."
             validation_extension = self.validation_file.split(".")[-1]
-            assert (
-                validation_extension == train_extension
-            ), "`validation_file` should have the same extension (csv or json) as `train_file`."
+            assert validation_extension == train_extension, "`validation_file` should have the same extension (csv or json) as `train_file`."
 
 
 @dataclass
@@ -187,48 +117,16 @@ class ModelArguments:
     Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
     """
 
-    model_name_or_path: str = field(
-        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
-    )
-    freeze_model: bool = field(
-        default=False,
-        metadata={"help": "Whether to freeze the parameters of the base model."}
-    )
-    config_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
-    )
-    tokenizer_name: Optional[str] = field(
-        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
-    )
-    cache_dir: Optional[str] = field(
-        default=None,
-        metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
-    )
-    use_fast_tokenizer: bool = field(
-        default=True,
-        metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."},
-    )
-    model_revision: str = field(
-        default="main",
-        metadata={"help": "The specific model version to use (can be a branch name, tag name or commit id)."},
-    )
-    use_auth_token: bool = field(
-        default=False,
-        metadata={
-            "help": (
-                "Will use the token generated when running `huggingface-cli login` (necessary to use this script "
-                "with private models)."
-            )
-        },
-    )
-    ignore_mismatched_sizes: bool = field(
-        default=False,
-        metadata={"help": "Will enable to load a pretrained model whose head dimensions are different."},
-    )
-    trust_remote_code: bool = field(
-        default=False,
-        metadata={"help": "Whether to allow external code to be executed when running this script."},
-    )
+    model_name_or_path: str = field(metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"})
+    freeze_model: bool = field(default=False, metadata={"help": "Whether to freeze the parameters of the base model."})
+    config_name: Optional[str] = field(default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"})
+    tokenizer_name: Optional[str] = field(default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"})
+    cache_dir: Optional[str] = field(default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"})
+    use_fast_tokenizer: bool = field(default=True, metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."})
+    model_revision: str = field(default="main", metadata={"help": "The specific model version to use (can be a branch name, tag name or commit id)."})
+    use_auth_token: bool = field(default=False, metadata={"help": "Will use the token generated when running `huggingface-cli login` (necessary to use this script with private models)."})
+    ignore_mismatched_sizes: bool = field(default=False, metadata={"help": "Will enable to load a pretrained model whose head dimensions are different."})
+    trust_remote_code: bool = field(default=False, metadata={"help": "Whether to allow external code to be executed when running this script."})
 
 
 def main():
@@ -250,8 +148,7 @@ def main():
         training_args.save_total_limit = 1
         training_args.load_best_model_at_end = True
         training_args.evaluation_strategy = "epoch"
-        callbacks = [EarlyStoppingCallback(early_stopping_patience=data_args.patience,
-                                           early_stopping_threshold=0.001)]
+        callbacks = [EarlyStoppingCallback(early_stopping_patience=data_args.patience, early_stopping_threshold=0.001)]
     else:
         callbacks = None
 
@@ -274,10 +171,7 @@ def main():
     transformers.utils.logging.enable_explicit_format()
 
     # Log on each process the small summary:
-    logger.warning(
-        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
-        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
-    )
+    logger.warning(f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}, distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}")
     logger.info(f"Training/evaluation parameters {training_args}")
 
     # Detecting last checkpoint.
@@ -285,15 +179,9 @@ def main():
     if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
         last_checkpoint = get_last_checkpoint(training_args.output_dir)
         if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
-            raise ValueError(
-                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
-                "Use --overwrite_output_dir to overcome."
-            )
+            raise ValueError(f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome.")
         elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
-            logger.info(
-                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
-                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
-            )
+            logger.info(f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change the `--output_dir` or add `--overwrite_output_dir` to train from scratch.")
 
     # Set seed before initializing model.
     set_seed(training_args.seed)
@@ -337,16 +225,12 @@ def main():
             if data_args.test_file is not None:
                 train_extension = data_args.train_file.split(".")[-1]
                 test_extension = data_args.test_file.split(".")[-1]
-                assert (
-                    test_extension == train_extension
-                ), "`test_file` should have the same extension (csv or json) as `train_file`."
+                assert test_extension == train_extension, "`test_file` should have the same extension (csv or json) as `train_file`."
                 data_files["test"] = data_args.test_file
             else:
                 train_extension = data_args.train_file.split(".")[-1]
                 validation_extension = data_args.validation_file.split(".")[-1]
-                assert (
-                    validation_extension == train_extension
-                ), "`validation_file` should have the same extension (csv or json) as `train_file`."
+                assert validation_extension == train_extension, "`validation_file` should have the same extension (csv or json) as `train_file`."
                 data_files["test"] = data_args.validation_file
 
         for key in data_files.keys():
@@ -414,6 +298,10 @@ def main():
         trust_remote_code=model_args.trust_remote_code,
         use_auth_token=True if model_args.use_auth_token else None,
     )
+
+    if model_args.model_name_or_path.startswith("meta-llama") or model_args.model_name_or_path.startswith("gpt2") or model_args.model_name_or_path.startswith("EleutherAI"):
+        tokenizer.pad_token = tokenizer.eos_token
+
     model = AutoModelForSequenceClassification.from_pretrained(
         model_args.model_name_or_path,
         from_tf=bool(".ckpt" in model_args.model_name_or_path),
@@ -502,10 +390,7 @@ def main():
         model.config.id2label = {id: label for label, id in config.label2id.items()}
 
     if data_args.max_seq_length > tokenizer.model_max_length:
-        logger.warning(
-            f"The max_seq_length passed ({data_args.max_seq_length}) is larger than the maximum length for the"
-            f"model ({tokenizer.model_max_length}). Using max_seq_length={tokenizer.model_max_length}."
-        )
+        logger.warning(f"The max_seq_length passed ({data_args.max_seq_length}) is larger than the maximum length for the model ({tokenizer.model_max_length}). Using max_seq_length={tokenizer.model_max_length}.")
     max_seq_length = min(data_args.max_seq_length, tokenizer.model_max_length)
 
     def preprocess_function(examples):
@@ -513,13 +398,10 @@ def main():
         if isinstance(sentence1_key, list):
             keya, keyb = examples[sentence1_key[0]], examples[sentence1_key[1]]
             keys1 = [template.format(ka, kb) for ka, kb in zip(keya, keyb)]
-            args = (
-                (keys1, examples[sentence2_key])
-            )
+            args = (keys1, examples[sentence2_key])
         else:
-            args = (
-                (examples[sentence1_key],) if sentence2_key is None else (examples[sentence1_key], examples[sentence2_key])
-            )
+            args = (examples[sentence1_key],) if sentence2_key is None else (examples[sentence1_key], examples[sentence2_key])
+
         result = tokenizer(*args, padding=padding, max_length=max_seq_length, truncation=True)
 
         # Map labels to IDs (not necessary for GLUE tasks)
@@ -627,9 +509,7 @@ def main():
             checkpoint = last_checkpoint
         train_result = trainer.train(resume_from_checkpoint=checkpoint)
         metrics = train_result.metrics
-        max_train_samples = (
-            data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
-        )
+        max_train_samples = data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
         metrics["train_samples"] = min(max_train_samples, len(train_dataset))
 
         trainer.save_model()  # Saves the tokenizer too for easy upload
@@ -659,9 +539,7 @@ def main():
                 eval_dataset = eval_dataset.filter(lambda example: example["condition"] == "test")
             metrics = trainer.evaluate(eval_dataset=eval_dataset)
 
-            max_eval_samples = (
-                data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
-            )
+            max_eval_samples = data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
             metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
 
             if task == "mnli-mm":
--- finetune_model.sh
+++ finetune_model.sh
@@ -1,44 +1,71 @@
 #!/bin/bash
 
-MODEL_PATH=$1
-LR=${2:-5e-5}           # default: 5e-4
-PATIENCE=${3:-3}       # default: 3
-BSZ=${4:-64}            # default: 64
-MAX_EPOCHS=${5:-10}     # default: 10
-SEED=${6:-12}           # default: 12
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=62GB
+#SBATCH --time=1:00:00
+#SBATCH --job-name=eval_glue
+#SBATCH --output=eval_glue_%A_%a.out
+#SBATCH --array=0
 
-model_basename=$(basename $MODEL_PATH)
-for task in {boolq,cola,mnli,mnli-mm,mrpc,multirc,qnli,qqp,rte,sst2,wsc}; do
-	if [[ $task = "mnli-mm" ]]; then
-		TRAIN_NAME="mnli"
-		VALID_NAME="mnli-mm"
-		DO_TRAIN=False
-		MODEL_PATH_FULL="results/finetune/$model_basename/$TRAIN_NAME/"
-	else
-		TRAIN_NAME=$task
-		VALID_NAME=$task
-		DO_TRAIN=True
-		MODEL_PATH_FULL=$MODEL_PATH
-	fi
+# define models
+MODEL_NAMES=(
+    "babylm_10M"
+    "wikipedia_10M_1" "wikipedia_10M_2" "wikipedia_10M_3"
+    "gutenberg_10M_1" "gutenberg_10M_2" "gutenberg_10M_3"
+    "tinystories_10M_1" "tinystories_10M_2" "tinystories_10M_3"
+    "pythonedu_10M_1" "pythonedu_10M_2" "pythonedu_10M_3"
+    "babylm_100M"
+    "wikipedia_100M_1" "wikipedia_100M_2" "wikipedia_100M_3"
+    "gutenberg_100M_1" "gutenberg_100M_2" "gutenberg_100M_3"
+    "tinystories_100M_1" "tinystories_100M_2" "tinystories_100M_3"
+    "pythonedu_100M_1" "pythonedu_100M_2" "pythonedu_100M_3"
+)
 
-	mkdir -p results/finetune/$model_basename/$task/
+# get current model name based on array index
+MODEL_NAME=${MODEL_NAMES[$SLURM_ARRAY_TASK_ID]}
+BASE_MODEL_PATH=/scratch/eo41/babylm/models/${MODEL_NAME}
 
-	python finetune_classification.py \
-	  --model_name_or_path $MODEL_PATH_FULL \
-	  --output_dir results/finetune/$model_basename/$task/ \
-	  --train_file evaluation_data/glue_filtered/$TRAIN_NAME.train.jsonl \
-	  --validation_file evaluation_data/glue_filtered/$VALID_NAME.valid.jsonl \
-	  --do_train $DO_TRAIN \
-	  --do_eval \
-	  --do_predict \
-	  --max_seq_length 128 \
-	  --per_device_train_batch_size $BSZ \
-	  --learning_rate $LR \
-	  --num_train_epochs $MAX_EPOCHS \
-	  --patience $PATIENCE \
-	  --evaluation_strategy epoch \
-	  --save_strategy epoch \
-	  --overwrite_output_dir \
-	  --trust_remote_code \
-	  --seed $SEED
+# loop through step directories within current model
+for FULL_MODEL_PATH in ${BASE_MODEL_PATH}/step_*; do
+
+    echo "Model: ${FULL_MODEL_PATH}"
+
+	STEP_NAME=$(basename $FULL_MODEL_PATH)
+	for TASK in {boolq,cola,mnli,mnli-mm,mrpc,multirc,qnli,qqp,rte,sst2,wsc}; do
+		if [[ $TASK = "mnli-mm" ]]; then
+			TRAIN_NAME="mnli"
+			VALID_NAME="mnli-mm"
+			DO_TRAIN=False
+			MODEL_PATH_FULL="results/finetune/$MODEL_NAME/$STEP_NAME/$TRAIN_NAME/"
+		else
+			TRAIN_NAME=$TASK
+			VALID_NAME=$TASK
+			DO_TRAIN=True
+			MODEL_PATH_FULL=$FULL_MODEL_PATH
+		fi
+
+		mkdir -p results/finetune/$MODEL_NAME/$STEP_NAME/$TASK/
+
+		python finetune_classification.py \
+		--model_name_or_path $MODEL_PATH_FULL \
+		--output_dir results/finetune/$MODEL_NAME/$STEP_NAME/$TASK/ \
+		--train_file evaluation_data/glue_filtered/$TRAIN_NAME.train.jsonl \
+		--validation_file evaluation_data/glue_filtered/$VALID_NAME.valid.jsonl \
+		--do_train $DO_TRAIN \
+		--do_eval \
+		--do_predict \
+		--max_seq_length 128 \
+		--per_device_train_batch_size 64 \
+		--learning_rate 0.0003 \
+		--num_train_epochs 10 \
+		--patience 3 \
+		--evaluation_strategy epoch \
+		--save_strategy epoch \
+		--overwrite_output_dir \
+		--trust_remote_code \
+		--seed 1
+	done
 done
